0ef90f169e3986179be9fef3623059657fb525fe
Jane94 <39977920+huynhmy6@users.noreply.github.com>
2019-06-23 15:42:32 +0800

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,23 @@
+alert("Hey! I have a question for you");
+
+alert("Don't be a Liar : ....!");
+
+function move(){
+
+    var x = Math.random()* 200
+    var y = Math.random()*200
+    var left = x+'px'
+    var top = y+'px'
+    document.getElementById("button_No").style.left = left;
+    document.getElementById("button_No").style.top = top;
+
+}
+
+function yah_Yeu(){
+
+           alert("Huh? REALLY?")
+           alert("I KNEW YOU WERE STUPID, ACTUALLY!")
+           alert("DON'T TRY TO PROVE THAT YOU AIN'T STUPID BY CLICKING NO BUTTON")
+           alert("POOR YOU! IGNORAMUS =)))!")
+}
+
